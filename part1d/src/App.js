6215a6aeb68998a1dc5557b31a6c47f4e8b876e4
Regan Lam <regan_lam@hotmail.com>
2021-08-15 14:09:17 -0700

    part1d left right example

---
+++ part1d/src/App.js
@@ -0,0 +1,45 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>;
+  }
+  return <div>button press history: {props.allClicks.join(" ")}</div>;
+};
+
+const Button = ({ handleClick, text }) => {
+  return (
+    <>
+      <button onClick={handleClick}>{text}</button>
+    </>
+  );
+};
+
+const App = () => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+
+  const handleLeftClick = () => {
+    debugger;
+    setAll(allClicks.concat("L"));
+    setLeft(left + 1);
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    setRight(right + 1);
+  };
+
+  return (
+    <div>
+      {left}
+      <Button handleClick={handleLeftClick} text={"left"} />
+      <Button handleClick={handleRightClick} text={"right"} />
+      <History allClicks={allClicks} />
+    </div>
+  );
+};
+export default App;
