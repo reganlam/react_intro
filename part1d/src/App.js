c79e3dab273854b0fa2e10b9d4adc2c5b911989c
Regan Lam <regan_lam@hotmail.com>
2021-08-15 15:12:37 -0700

    ex1.6 to ex1.11

--- part1d/src/App.js
+++ part1d/src/App.js
@@ -1,45 +1,70 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { useState } from "react";
+import React, { useState } from "react";
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return <div>the app is used by pressing the buttons</div>;
-  }
-  return <div>button press history: {props.allClicks.join(" ")}</div>;
-};
-
-const Button = ({ handleClick, text }) => {
+const Button = ({ state, setState, text }) => {
   return (
     <>
-      <button onClick={handleClick}>{text}</button>
+      <button onClick={() => setState(state + 1)}>{text}</button>
+    </>
+  );
+};
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <>
+      <tr>
+        <td>{text}</td>
+        <td>{value}</td>
+      </tr>
+    </>
+  );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = good / all;
+
+  if (all === 0)
+    return (
+      <>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </>
+    );
+
+  return (
+    <>
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={positive} />
+        </tbody>
+      </table>
     </>
   );
 };
 
 const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
-
-  const handleLeftClick = () => {
-    debugger;
-    setAll(allClicks.concat("L"));
-    setLeft(left + 1);
-  };
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat("R"));
-    setRight(right + 1);
-  };
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
-      {left}
-      <Button handleClick={handleLeftClick} text={"left"} />
-      <Button handleClick={handleRightClick} text={"right"} />
-      <History allClicks={allClicks} />
+      <h1>give feedback here</h1>
+      <Button state={good} setState={setGood} text="good" />
+      <Button state={neutral} setState={setNeutral} text="neutral" />
+      <Button state={bad} setState={setBad} text="bad" />
+
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
+
 export default App;
