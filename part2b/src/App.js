9b273dd0e35dbada165c42250c15ed109fedc8b4
Regan Lam <regan_lam@hotmail.com>
2021-08-24 15:41:03 -0700

    pt2 ex2.11

--- part2b/src/App.js
+++ part2b/src/App.js
@@ -1,21 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import Note from "./components/Note";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
-const App = () => {
+function App() {
   // List of people
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
 
   // Form data
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newSearch, setNewSearch] = useState("");
+
+  useEffect(() => {
+    const eventHandler = (res) => {
+      setPersons(res.data);
+    };
+
+    const promise = axios.get("http://localhost:3001/persons");
+    promise.then(eventHandler);
+  }, []);
+  console.log(`Fetched ${persons.length} people`);
 
   // onSubmit
   const addPerson = (e) => {
@@ -73,6 +81,6 @@ const App = () => {
       <Persons persons={persons} filter={newSearch} />
     </div>
   );
-};
+}
 
 export default App;
