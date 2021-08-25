24d05ca0d24b834c9bc1bc8bed9974f52c790501
Regan Lam <regan_lam@hotmail.com>
2021-08-24 18:51:09 -0700

    p2d

---
+++ part2d/src/App.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Note from "./components/Note";
+import noteService from "./services/notes";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
+
+  useEffect(() => {
+    noteService.getAll().then((res) => {
+      setNotes(res);
+    });
+  }, []);
+
+  console.log(notes);
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+
+    noteService.create(noteObject).then((res) => {
+      setNotes(notes.concat(res));
+      setNewNote("");
+    });
+  };
+
+  const handleNoteChange = (e) => {
+    setNewNote(e.target.value);
+  };
+
+  const toggleImportanceOf = (id) => {
+    console.log("importance of " + id + " needs to be toggled");
+
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    noteService.update(id, changedNote).then((res) => {
+      setNotes(notes.map((note) => (note.id !== id ? note : res))).catch(
+        (error) => {
+          alert(`the note '${note.content}' was already deleted from server`);
+          setNotes(notes.filter((n) => n.id !== id));
+        }
+      );
+    });
+  };
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note, i) => (
+          <Note
+            key={i}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        ))}
+      </ul>
+
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
---
+++ part2d/src/services/notes.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3001/notes";
+
+const getAll = () => {
+	const api = axios.get(baseUrl);
+	return api.then((res) => res.data);
+};
+
+const create = (newObject) => {
+	const api = axios.post(baseUrl, newObject);
+	return api.then((res) => res.data);
+};
+
+const update = (id, newObject) => {
+	const api = axios.put(`${baseUrl}/${id}`, newObject);
+	return api.then((res) => res.data);
+};
+
+export default {
+	getAll,
+	create,
+	update,
+};
